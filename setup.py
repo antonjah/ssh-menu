2cdfb1910c50ecaf2f9396ddb7f4cb1722511df5
Anton Andersson <anton.andersson@protegrity.com>
2019-01-16 11:55:59 +0100

    Add classifiers to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 try:
     from setuptools import setup
 except ImportError:
@@ -8,20 +7,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 config = {
-    'description': 'ssh-menu',
-    'keywords': 'ssh menu interactive',
-    'author': 'Anton Andersson',
-    'description': 'A very simple interactive ssh profile menu',
-    'long_description': long_description,
-    'long_description_content_type': 'text/markdown',
-    'url': 'https://github.com/antonjah/ssh-menu',
-    'download_url': 'https://github.com/antonjah/ssh-menu',
-    'author_email': 'contact@antonandersson.se',
-    'version': '1.2.2',
-    'install_requires': ['console-menu', 'sshconf'],
-    'packages': ['ssh-menu'],
-    'scripts': ['bin/ssh-menu'],
-    'name': 'ssh-menu'
+    "keywords": "ssh menu interactive",
+    "author": "Anton Andersson",
+    "description": "A very simple interactive ssh profile menu",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "url": "https://github.com/antonjah/ssh-menu",
+    "download_url": "https://github.com/antonjah/ssh-menu",
+    "author_email": "contact@antonandersson.se",
+    "version": "1.2.2",
+    "install_requires": ["console-menu", "sshconf"],
+    "packages": ["ssh-menu"],
+    "scripts": ["bin/ssh-menu"],
+    "name": "ssh-menu",
+    "classifiers": [
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2.7",
+    ],
 }
 
 setup(**config)
